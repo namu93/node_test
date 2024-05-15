db70cd47ddc597e2ce98972709441f2a08f16e0c
namu93 <111473555+namu93@users.noreply.github.com>
2024-05-15 23:09:08 +0900

    first commit
    처음올리는 동물 울음소리 api

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/sound/:name", (req, res) => {
+  const { name } = req.params;
+  console.log(name);
+  if (name == "dog") {
+    res.json({ sound: "멍멍" });
+  } else if (name == "cat") {
+    res.json({ sound: "야옹" });
+  } else if (name == "pig") {
+    res.json({ sound: "꿀꿀" });
+  } else {
+    res.json({ sound: "알 수 없음" });
+  }
+});
+
+app.get("/cat", (req, res) => {
+  res.send({ sound: "냐옹" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
